4fab6c3523c4b19158f4d92ae0ecb431d2fc0c64
Santi Espinosa <saigesp@gmail.com>
2021-02-08 20:38:56 +0100

    rename examples folder

--- ejemplos/dafont/dafont.py
+++ ejemplos/dafont/dafont.py
@@ -1,18 +1,20 @@
-import urllib2
+from urllib import request
 from bs4 import BeautifulSoup
 
 url = 'https://www.dafont.com/es/theme.php?cat=301'
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
 }
 
 # Get URL
-response = urllib2.Request(url, headers=headers)
-pagedata = urllib2.urlopen(response)
+response = request.Request(url, headers=headers)
+pagedata = request.urlopen(response)
 html = pagedata.read()
 
 # Get links
 soup = BeautifulSoup(html, "html.parser")
 links = soup.select('.lv1left')
 
-print len(links)
+for link in links:
+    print(link.text) # Print link title
