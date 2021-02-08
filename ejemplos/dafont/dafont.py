6bc0d25aecaa05a25c72650bba9fc57b41d4c733
Santi Espinosa <saigesp@gmail.com>
2021-02-08 20:44:50 +0100

    update dafont example

--- ejemplos/dafont/dafont.py
+++ ejemplos/dafont/dafont.py
@@ -1,7 +1,13 @@
 from urllib import request
 from bs4 import BeautifulSoup
 
+"""
+Script para listar todas las tipografías de Dafont
+en la categoría Tecno > Cuadrado (cat=301)
+"""
+
 url = 'https://www.dafont.com/es/theme.php?cat=301'
+font_url = 'https://www.dafont.com/es/'
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
@@ -17,4 +23,6 @@ soup = BeautifulSoup(html, "html.parser")
 links = soup.select('.lv1left')
 
 for link in links:
-    print(link.text) # Print link title
+    print('Name:', link.text.replace('  ', ' ')) # Print title
+    print('Link:', font_url + link.select('a')[0]['href'])
+    print()
